ad77fe2e2fbba4c97fa41f33140f62a31f2e2617
Diulia Granatto <diuliagranatto@gmail.com>
2020-05-14 19:17:39 -0300

    Leitura dos outros dados

--- main.cpp
+++ main.cpp
@@ -11,8 +11,19 @@ int main()
   cout << "1) Ensino Fundamental" << endl;
   cout << "2) Ensino Medio" << endl;
   cout << "3) Ensino Superior" << endl;
-  cout << "4) Pos-graduação" << endl;
+  cout << "4) Pos-graduacao" << endl;
   cout << "Digite uma opcao: ";
    cin >> escolaridade;
+
+   int experiencia;
+   string disponibilidadeViagem, habilitacao;
+
+   cout << "Voce tem quantos anos de experiencia profissional? ";
+   cin >> experiencia;
+   cout << "Voce tem disponibilidade para viajar (S/N)? ";
+   cin >> disponibilidadeViagem;
+   cout << "Voce tem habilitacao de motorista (S/N)? ";
+   cin >> habilitacao;
+
     return 0;
 }
